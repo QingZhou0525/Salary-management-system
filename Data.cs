8238a754eb07da78269d0a5bb4aaa6cb10457d1e
QingZhouJiaJiu <43428185+QingZhou0525@users.noreply.github.com>
2020-04-19 12:07:47 +0800

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 工资管理系统
+{
+    public class Data
+    {
+        public static string id = null;
+    }
+}
